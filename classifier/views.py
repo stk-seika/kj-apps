ca42cdcf957c92f9bb4f7dd003de7da1c3e908f0
stk-seika <stk-seika@users.noreply.github.com>
2022-02-18 22:54:04 +0900

    label to name

--- classifier/views.py
+++ classifier/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from django.conf import settings
 from django import http
-from PIL import Image
+import json
 import io
 import base64
+from PIL import Image
 
 from .forms import ImageForm
 from classifier import classifier
@@ -12,7 +14,7 @@ from classifier import classifier
 class ClassifierView(generic.TemplateView):
 	template_name = 'classifier/classifier.html'
 	classifier_model = classifier.init_model()
-
+		
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 		context['form'] = ImageForm()
@@ -21,17 +23,53 @@ class ClassifierView(generic.TemplateView):
 		context['image'] = None
 		context['alert'] = None
 		self.classifier_model = classifier.init_model()
+
 		return context
 
 	def get(self, request, *args, **kwargs) -> http.HttpResponse:
 		return super().get(request, *args, **kwargs)
+
+	# label to name
+	# label ID から名前を返す
+	def label_to_name(self, labels, chara_table):
+		# リストの場合
+		if type(labels) == list or type(labels) == tuple:
+			names = []
+			for label in labels:
+				hit = False
+				try:
+					for k, v in chara_table.items():
+						if v['id'] == int(label):
+							names.append(k)
+							hit = True
+							break
+					if not hit:
+						names.append("")
+				# int()でintに変換出来ないとき
+				except ValueError:
+					names.append("")
+			return names
+		# intかstrのとき
+		elif type(labels) == int or type(labels) == str:
+			name = ""
+			try:
+				for k, v in chara_table.items():
+					if v['id'] == int(labels):
+						name = k
+						break
+			except ValueError:
+				pass
+			return name
+		# その他のとき
+		else:
+			return ""
 
 	def post(self, request, *args, **kwargs):
 		form = ImageForm(request.POST, request.FILES)
 		if not form.is_valid():
 			context = self.get_context_data()
 
-			# Only file size <= 50MB
+			# Only file size <= 50MB is acceptable
 			if 'File size over' in form.errors['image']:
 				context['alert'] = '50MB以下のファイルのみ実行します'
 			else:
@@ -50,6 +88,13 @@ class ClassifierView(generic.TemplateView):
 		img = img.decode("utf-8")	# decode to image
 		self.kwargs['image'] = img
 
+		# Character name string
+		# table_file = 'classifier/static/classifier/chara_table.json'
+		table_file = settings.STATIC_ROOT + 'classifier/chara_table.json'
+		chara_table = dict()
+		with open(table_file, "r") as f:
+			chara_table = json.load(f)
+
 		# Generate return values
 		preds = classifier.pred(image, model=self.classifier_model)
 		top_labels = {}
@@ -60,7 +105,7 @@ class ClassifierView(generic.TemplateView):
 			# 一つ前の半分以下か差が0.005以下なら表示しない
 			if (n > 3) and (value < 0.1) and (((value_pre / value) > 2.0) or (value_pre - value < 0.005)):
 				break
-			top_labels[str(idx)] = value
+			top_labels[self.label_to_name(int(idx), chara_table)] = value
 			value_pre = value
 		self.kwargs['pred'] = top_labels
 		self.kwargs['done'] = True
